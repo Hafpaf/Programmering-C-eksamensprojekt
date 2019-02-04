059b723759bbd6092deaa03fd8e6f2a420f1615c
hafpaf <haf@hafnium.me>
2019-02-04 13:05:15 +0100

    init commit

---
+++ rsa_test.py
@@ -0,0 +1,11 @@
+import random
+#Prime one: 7213
+#Prime two 102337
+
+#def primegen():
+#input(prime1,prime2:, , )#input, change later to prime generator
+prime_p = 7213
+prime_q = 102337
+
+primes = [prime_p,prime_q] #create array with primes
+print(primes)
