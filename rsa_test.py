dd36a4acf65d14d429f554dba2f7b052f62b8a48
hafpaf <haf@hafnium.me>
2019-02-05 19:50:32 +0100

    array working

--- rsa_test.py
+++ rsa_test.py
@@ -1,5 +1,5 @@
 import random
-import array
+from array import array #using array function
 #Prime one: 7213
 #Prime two 102337
 
@@ -8,5 +8,6 @@ import array
 prime_p = 7213
 prime_q = 102337
 
-class array.primes(I[prime_p,prime_q]) #big I means unsigned integer. Create array with primes
-print(primes)
+primes = array('I',[prime_p,prime_q]) #big I means unsigned integer. Create array with primes
+for primeslist in primes:
+    print(primeslist)
