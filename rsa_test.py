668d67ffdd5442f9e226c4c62cda32a72b4b8f07
hafpaf <haf@hafnium.me>
2019-02-05 20:38:50 +0100

    first gcd test

--- rsa_test.py
+++ rsa_test.py
@@ -1,5 +1,9 @@
+'''
+Generate rsa keypair
+'''
 import random
 from array import array #using array function
+from math import gcd
 #Prime one: 7213
 #Prime two 102337
 
@@ -11,3 +15,18 @@ prime_q = 102337
 primes = array('I',[prime_p,prime_q]) #big I means unsigned integer. Create array with primes
 for primeslist in primes:
     print(primeslist)
+
+n = prime_p * prime_q #calculate n
+phi_n = (prime_p - 1) * (prime_q - 1) #calculate phi_n
+
+number_e = 352841
+
+gcd_calc = gcd(phi_n,number_e) #must return 1
+
+while True:
+    if gcd_calc == 1: #This is to make sure prime numbers are used since any prime will return 1
+        print("gcd_calc: True")
+        break
+    else:
+        print("gcd_calc: False")
+        break
