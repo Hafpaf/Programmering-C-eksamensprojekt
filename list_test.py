a2db6c225d97f7e469c2167d28545ebc20339d16
hafpaf <haf@hafnium.me>
2019-04-26 11:33:23 +0200

    add user input and comments

--- list_test.py
+++ list_test.py
@@ -12,21 +12,24 @@ for i in range(10):
     prime_array.append(foo)
 '''
 
+interval = int(input("Generate primes below: "))
+
+#interval = 1000000 #100k
 def prime_limit():
-    counter = 0 #count times ran
-    interval = 1000000 #100k
+    #counter = 0 #count times ran
+    #interval = 1000000 #100k
     for i in gen_primes():
         if i < interval: #<-- below this number
-            counter +=1
+            #counter +=1
             last_prime = i
             prime_array.append(last_prime)
             #print("Prime number", counter, "is:", i) #prit all outputs, comment to optimize
         else:
-            print("The last Prime number below",interval)
-            print("Prime number", counter, "is:", last_prime)
+            #print("The last Prime number below",interval)
+            #print("Prime number", counter, "is:", last_prime)
             break
 
 prime_limit()
 #print(prime_array)
 foo = random.choice(prime_array)
-print("Random prime: ",foo)
+print('Random prime below {} is: '.format(interval) ,foo)
