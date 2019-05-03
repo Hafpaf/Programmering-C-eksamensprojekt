349f2ffcdf70e3866042df37f13eed672fde73b7
hafpaf <haf@hafnium.me>
2019-05-03 12:44:24 +0200

    crypto generate random int

--- SystemRandom_test.py
+++ SystemRandom_test.py
@@ -1,18 +1,11 @@
-'''import random
-import os
-s = random.SystemRandom(1)
-#i = int(s,16)
-#string = str(i)
-#foo = os.urandom(10)
-foo = os.urandom(10)
-bar = random.randint(1,5)
-print(foo)'''
-
 import random
+import os
 
-sysrandom = random.SystemRandom()
-some_string = "123456789"
+'''sysrandom = os.urandom(24) #24 bytes
+convert_to_hex = sysrandom.hex()
+convert_to_int = int(sysrandom.hex(), 16) #base 16'''
+#cryptographily_random_int = int(os.urandom(24).hex(), 16)
 #print(sysrandom.choice(some_string)) # print random character from the string
-for i in range():
-    range_from_string = some_string[sysrandom.randrange(1,5)]
-    print(range_from_string) # same
+for i in range(10):
+    cryptographily_random_int = int(os.urandom(24).hex(), 16)
+    print(cryptographily_random_int)
